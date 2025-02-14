e661b90e7f7b0233f0aeb77c91b97abf27a48193
SHARANYASWANT <sharanyaswant.06@gmail.com>
2025-02-14 13:52:10 +0530

    Added middleware 	modified:   middleware/validateCsrf.js 	modified:   middleware/validateSession.js

--- middleware/validateCsrf.js
+++ middleware/validateCsrf.js
@@ -1,14 +1,29 @@
-export function validateCsrf(req, res, next) {
-    const csrfToken = req.cookies.csrfToken;
-  
-    if (!csrfToken) {
-      return res.status(401).json({ error: 'Invalid CSRF token' });
-    }
-  
-    if (csrfToken !== req.cookies.csrfToken) {
-      return res.status(401).json({ error: 'Invalid CSRF token' });
-    }
-  
-    next();
+import pool from '../config/db.js';
+
+export async function validateCsrf(req, res, next) {
+  const csrfToken = req.cookies.csrfToken;
+  const sessionID = req.cookies.sessionID;
+
+  if (!csrfToken || !sessionID) {
+    return res.status(401).json({ error: 'Invalid CSRF token or Session ID' });
   }
-  
+
+  try {
+    const result = await pool.query('SELECT csrf_token FROM sessions WHERE session_id = $1', [sessionID]);
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ error: 'Invalid CSRF token' });
+    }
+
+    const storedCsrfToken = result.rows[0].csrf_token;
+
+    if (csrfToken !== storedCsrfToken) {
+      return res.status(403).json({ error: 'Invalid CSRF token' });
+    }
+
+    next();
+  } catch (error) {
+    console.error('CSRF token validation error:', error);
+    res.status(500).json({ error: 'CSRF validation failed' });
+  }
+}
--- middleware/validateSession.js
+++ middleware/validateSession.js
@@ -4,20 +4,28 @@ export async function validateSession(req, res, next) {
   const sessionID = req.cookies.sessionID;
 
   if (!sessionID) {
-    return res.status(401).json({ error: 'Session expired or invalid' });
+    return res.status(401).json({ error: 'Invalid session' });
   }
 
   try {
     const result = await pool.query('SELECT * FROM sessions WHERE session_id = $1', [sessionID]);
 
     if (result.rows.length === 0) {
-      return res.status(401).json({ error: 'Session expired or invalid' });
+      return res.status(401).json({ error: 'Invalid session' });
     }
 
     const session = result.rows[0];
+
     if (new Date() > new Date(session.expires_at)) {
-    //   await pool.query('DELETE FROM sessions WHERE session_id = $1', [sessionID]);
-      return res.status(401).json({ error: 'Session expired or invalid' });
+      await pool.query('DELETE FROM sessions WHERE session_id = $1', [sessionID]);
+      res.clearCookie('sessionID'); 
+      res.clearCookie('csrfToken'); 
+      res.clearCookie('userId');
+      return res.status(401).json({ error: 'Session expired' });
+    }
+
+    if(session.sessionID !== sessionID){
+      return res.status(401).json({ error: 'Invalid session' });
     }
 
     req.userId = session.user_id;
